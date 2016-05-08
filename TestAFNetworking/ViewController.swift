b116c460cc58bb0b37659a3313e6915a3212687f
矢野史洋 <a2.mj.pop@gmail.com>
2016-05-08 18:14:46 +0900

    add Api通信で値を取得できるようにした。

--- TestAFNetworking/ViewController.swift
+++ TestAFNetworking/ViewController.swift
@@ -8,16 +8,28 @@
 
 import UIKit
 
+private var json:NSDictionary!
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.redColor()
         let manager = AFHTTPRequestOperationManager()
-//        manager.requestSerializer.setValue("api-key", forHTTPHeaderField: "test_key")
+        manager.requestSerializer.setValue("test_key", forHTTPHeaderField: "api-key")
+        manager.responseSerializer = AFHTTPResponseSerializer()
         manager.GET("http://localhost:3000/api/items", parameters: nil,
             success: {(operation, responseObject) in
-                print("Success! \(responseObject)")
+                do {
+                    json = try NSJSONSerialization.JSONObjectWithData(responseObject as! NSData, options: .MutableContainers) as! NSDictionary
+                    let items = json["items"] as! NSArray
+                    let item = items.objectAtIndex(0)
+                    let name = item["name"] as! NSString
+                    print(name)
+                } catch  {
+                    // エラー処理
+                }
+                print("Success! \(json)")
             },
             failure: {(operation, error) in
                 print("Error: \(error)")
